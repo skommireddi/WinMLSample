69c6f5172305cae2f6b8849d88f66625f7031b6d
Soumya Kommireddi <soumya_ks@hotmail.com>
2018-06-22 12:59:58 -0700

    Adding C# WinRT Component

--- WinMLSample/js/main.js
+++ WinMLSample/js/main.js
@@ -1,40 +1,8 @@
-﻿// loadModelFromStorageFileAsync() throws error
-(async () => {
-    var installedLoc = Windows.ApplicationModel.Package.current.installedLocation;
-    var modelsFolder = await installedLoc.getFolderAsync("Models");
-    var tiniyolovFolder = await modelsFolder.getFolderAsync("tiny_yolov2");
-    var modelFile = await tiniyolovFolder.getFileAsync("tiny_yolov2.onnx");
-    var model = await Windows.AI.MachineLearning.Preview.LearningModelPreview.loadModelFromStorageFileAsync(modelFile);
-    console.log(model.description);
-})();
+﻿if (window.Windows) {
+    Windows.UI.WebUI.WebUIApplication.addEventListener('activated', function (args) {
 
-
-// loadModelFromStorageFileAsync() does not throw error, it doesn't create model as expected
-//var modelFile = Windows.ApplicationModel.Package.current.installedLocation
-//    .getFolderAsync("Models")
-//    .then((folder) => folder.getFolderAsync("tiny_yolov2"))
-//    .then((folder) => folder.getFileAsync("tiny_yolov2.onnx"))
-//    .then((file) => Windows.AI.MachineLearning.Preview.LearningModelPreview.loadModelFromStorageFileAsync(file),
-//           error => console.log(error)
-//    )
-//    .then((model) => console.log(model), error => console.log(error), progress => console.log("loading"));
-
-
-// loadModelFromStorageFileAsync() throws error
-
-//var modelFile = Windows.ApplicationModel.Package.current.installedLocation
-//    .getFolderAsync("Models")
-//    .then((folder) => folder.getFolderAsync("tiny_yolov2"))
-//    .then((folder) => folder.getFileAsync("tiny_yolov2.onnx"))
-//    .then((file) => {
-//        Windows.AI.MachineLearning.Preview.LearningModelPreview.loadModelFromStorageFileAsync(file)
-//            .then(model => console.log(model), error => console.log(error));        
-//        }
-//    );
-
-
-
-
-
-
-
+        var mystr = WinRTBridgeComponent.Bridge.getAnswerStatic();
+        var bridge = new WinRTBridgeComponent.Bridge();
+        console.log(bridge.getAnswer());
+    });
+}
---
+++ WinRTBridgeComponent/Bridge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinRTBridgeComponent
+{
+    public sealed class Bridge
+    {
+        public string GetAnswer()
+        {
+            return "instance";
+        }
+
+        public static string GetAnswerStatic()
+        {
+            return "static";
+        }
+    }
+}
