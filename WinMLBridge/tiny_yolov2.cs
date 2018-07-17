6878bc7effee4f51c594d71c8faa9f79bfe0eb3e
Soumya Kommireddi <soumya_ks@hotmail.com>
2018-07-17 15:31:10 -0700

    Added logic to call IAsyncOperations in the win rt bridge from JS app

--- WinMLBridge/tiny_yolov2.cs
+++ WinMLBridge/tiny_yolov2.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Windows.Media;
 using Windows.Storage;
 using Windows.AI.MachineLearning.Preview;
+using Windows.Foundation;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 // d224f87054234871a169eda0b7463d86
 
@@ -25,15 +27,35 @@ namespace WinMLBridge
 
     public sealed class D224f87054234871a169eda0b7463d86Model
     {
-        public LearningModelPreview learningModel;
-        public static async Task<D224f87054234871a169eda0b7463d86Model> CreateD224f87054234871a169eda0b7463d86Model(StorageFile file)
+        private LearningModelPreview learningModel;
+
+        public static string CheckBridge()
         {
+            return "Can reach bridge";
+        }
+
+        public static IAsyncOperation<D224f87054234871a169eda0b7463d86Model> CreateModelAsync(Uri filePath)
+        {
+            return Task.Run(async () => { return await CreateD224f87054234871a169eda0b7463d86Model(filePath); }).AsAsyncOperation();           
+        }
+
+        public IAsyncOperation<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateModelAsync(D224f87054234871a169eda0b7463d86ModelInput input)
+        {
+            return Task.Run(async () => { return await EvaluateAsync(input); }).AsAsyncOperation();
+
+            // return AsyncInfo.Run(async _ => await EvaluateAsync(input)); this doesn't work 
+        }
+
+        private async static Task<D224f87054234871a169eda0b7463d86Model> CreateD224f87054234871a169eda0b7463d86Model(Uri filePath)
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(filePath);
             LearningModelPreview learningModel = await LearningModelPreview.LoadModelFromStorageFileAsync(file);
             D224f87054234871a169eda0b7463d86Model model = new D224f87054234871a169eda0b7463d86Model();
             model.learningModel = learningModel;
             return model;
         }
-        public async Task<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateAsync(D224f87054234871a169eda0b7463d86ModelInput input) {
+
+        private async Task<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateAsync(D224f87054234871a169eda0b7463d86ModelInput input) {
             D224f87054234871a169eda0b7463d86ModelOutput output = new D224f87054234871a169eda0b7463d86ModelOutput();
             LearningModelBindingPreview binding = new LearningModelBindingPreview(learningModel);
             binding.Bind("image", input.image);
--- WinMLSample/js/main.js
+++ WinMLSample/js/main.js
@@ -1,8 +1,23 @@
 ﻿if (window.Windows) {
-    Windows.UI.WebUI.WebUIApplication.addEventListener('activated', function (args) {
-      
-        var mystr1 = WinMLBridge.Bridge.hello();
+        Windows.UI.WebUI.WebUIApplication.addEventListener('activated', async function (args) {
 
-        console.log(mystr1);
-    });
+            var check = WinMLBridge.D224f87054234871a169eda0b7463d86Model.checkBridge();
+            console.log("Log:" + check);
+
+            await loadModel();
+        });
 }
+
+
+async function loadModel() {
+
+    var modelUri = new Windows.Foundation.Uri("ms-appx:///Models/tiny_yolov2/tiny_yolov2.onnx");
+
+    var model = await WinMLBridge.D224f87054234871a169eda0b7463d86Model.createModelAsync(modelUri);
+
+    console.log("Log: Model Loaded " + mystr1);
+}
+
+
+
+
