92d2d6d43a738fce58794eaa3e2aa66708b04f50
Soumya Kommireddi <soumya_ks@hotmail.com>
2018-08-07 16:45:54 -0700

    Adding logic to capture video from webcam Adding logic to snap photo and convert it to a video frame that the model accepts Switching  the onnx model to a working yolo model

--- WinMLBridge/tiny_yolov2.cs
+++ WinMLBridge/tiny_yolov2.cs
@@ -6,6 +6,10 @@ using Windows.Storage;
 using Windows.AI.MachineLearning.Preview;
 using Windows.Foundation;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.IO;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using System.Linq;
 
 // d224f87054234871a169eda0b7463d86
 
@@ -13,15 +17,15 @@ namespace WinMLBridge
 {
     public sealed class D224f87054234871a169eda0b7463d86ModelInput
     {
-        public VideoFrame image { get; set; }
+        public VideoFrame videoFrame { get; set; }
     }
 
     public sealed class D224f87054234871a169eda0b7463d86ModelOutput
     {
-        public IList<double> grid { get; set; }
+        public IList<float> grid { get; set; }
         public D224f87054234871a169eda0b7463d86ModelOutput()
         {
-            this.grid = new List<double>();
+            grid = new List<float>(new float[21125]);
         }
     }
 
@@ -40,9 +44,9 @@ namespace WinMLBridge
             return CreateD224f87054234871a169eda0b7463d86Model(modelUri).AsAsyncOperation();
         }
 
-        public IAsyncOperation<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateModelAsync(D224f87054234871a169eda0b7463d86ModelInput input)
+        public IAsyncOperation<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateModelAsync(string imageBase64Encoded)
         {
-           return EvaluateAsync(input).AsAsyncOperation(); 
+           return EvaluateAsync(imageBase64Encoded).AsAsyncOperation(); 
         }
 
         private async static Task<D224f87054234871a169eda0b7463d86Model> CreateD224f87054234871a169eda0b7463d86Model(Uri modelUri)
@@ -54,11 +58,35 @@ namespace WinMLBridge
             return model;
         }
 
-        private async Task<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateAsync(D224f87054234871a169eda0b7463d86ModelInput input) {
+        private async Task<D224f87054234871a169eda0b7463d86ModelOutput> EvaluateAsync(string imageBase64Encoded)
+        {
+
+            var imageBytes = Convert.FromBase64String(imageBase64Encoded);
+            var memoryStream = new MemoryStream(imageBytes);
+            BitmapDecoder bitDecoder = await BitmapDecoder.CreateAsync(memoryStream.AsRandomAccessStream());
+            var softwareBitmap = await bitDecoder.GetSoftwareBitmapAsync();
+            var videoFrame = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+            D224f87054234871a169eda0b7463d86ModelInput input = new D224f87054234871a169eda0b7463d86ModelInput
+            {
+                videoFrame = videoFrame
+            };         
+
+            // Retrieve model input and output variable descriptions (we already know the model takes an image in and outputs a tensor)
+            var inputFeatures = learningModel.Description.InputFeatures.ToList();
+            var outputFeatures = learningModel.Description.OutputFeatures.ToList();
+
+           var inputImageDescription =
+                inputFeatures.FirstOrDefault(feature => feature.ModelFeatureKind == LearningModelFeatureKindPreview.Image)
+                as ImageVariableDescriptorPreview;
+
+            var outputTensorDescription =
+                outputFeatures.FirstOrDefault(feature => feature.ModelFeatureKind == LearningModelFeatureKindPreview.Tensor)
+                as TensorVariableDescriptorPreview;
+
             D224f87054234871a169eda0b7463d86ModelOutput output = new D224f87054234871a169eda0b7463d86ModelOutput();
             LearningModelBindingPreview binding = new LearningModelBindingPreview(learningModel);
-            binding.Bind("image", input.image);
-            binding.Bind("grid", output.grid);
+            binding.Bind(inputImageDescription.Name, input.videoFrame);
+            binding.Bind(outputTensorDescription.Name, output.grid);
             LearningModelEvaluationResultPreview evalResult = await learningModel.EvaluateAsync(binding, string.Empty);
             return output;
         }
--- WinMLSample/js/main.js
+++ WinMLSample/js/main.js
@@ -4,18 +4,42 @@
             var check = WinMLBridge.D224f87054234871a169eda0b7463d86Model.checkBridge();
             console.log("Log:" + check);
 
-            await loadModel();
+            var video = document.getElementById('video');
+
+            // Get access to the camera!
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                // Not adding `{ audio: true }` since we only want video now
+                navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+                    video.src = window.URL.createObjectURL(stream);
+                    video.play();
+                });
+            }
+
+            // Elements for taking the snapshot
+            var canvas = document.getElementById('canvas');
+            var canvasContext = canvas.getContext('2d');
+
+            // Trigger photo take
+            document.getElementById("snap").addEventListener("click", async function () {
+                canvasContext.drawImage(video, 0, 0, 640, 480);               
+                var imageUrl = canvas.toDataURL("image/png");
+
+                var base64Image = imageUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+                var model = await loadModel();
+                var output = await model.evaluateModelAsync(base64Image);
+            });
         });
 }
 
 
 async function loadModel() {
+    //var yolo2 = "ms-appx:///Models/tiny_yolov2/tiny_yolov2.onnx";
+    var yolo = "ms-appx:///Models/tiny_yolov2/TinyYOLO.onnx";
 
-    var modelUri = new Windows.Foundation.Uri("ms-appx:///Models/tiny_yolov2/tiny_yolov2.onnx");
-
+    var modelUri = new Windows.Foundation.Uri(yolo);
     var model = await WinMLBridge.D224f87054234871a169eda0b7463d86Model.createModelAsync(modelUri);
-
-    console.log("Log: Model Loaded " + model);
+    console.log("Log: Model Loaded " + model);    
+    return model;
 }
 
 
