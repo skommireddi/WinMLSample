3141647cb06ba395053a8185e6806ca0a543dca9
Soumya Kommireddi <soumya_ks@hotmail.com>
2018-08-20 17:47:55 -0700

    Styling, adding code to upload photo WIP

--- WinMLSample/js/main.js
+++ WinMLSample/js/main.js
@@ -1,41 +1,117 @@
-﻿if (window.Windows) {
-        Windows.UI.WebUI.WebUIApplication.addEventListener('activated', async function (args) {
+﻿function openTab(tabName) {
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-            var check = WinMLBridge.YoloModel.checkBridge();
-            console.log("Log:" + check);
-
-            var video = document.getElementById('video');
-
-            // Get access to the camera!
-            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                // Not adding `{ audio: true }` since we only want video now
-                navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-                    video.src = window.URL.createObjectURL(stream);
-                    video.play();
-                });
-            }
-
-            // Elements for taking the snapshot
-            var canvas = document.getElementById('canvas');
-            var canvasContext = canvas.getContext('2d');
-
-            // Trigger photo take
-            document.getElementById("snap").addEventListener("click", async function () {
-                canvasContext.drawImage(video, 0, 0, 640, 480);               
-                var imageUrl = canvas.toDataURL("image/png");
-                var base64Image = imageUrl.replace(/^data:image\/(png|jpg);base64,/, "");
-
-                var image = new Image();
-                image.src = imageUrl;
-
-                var model = await loadModel();
-                var boxes = await model.evaluateModelAndProcessOutputAsync(base64Image);
-
-                renderImageOutput(boxes, image);
-            });
-        });
+    loadTab(tabName); 
 }
 
+
+if (window.Windows) {
+    Windows.UI.WebUI.WebUIApplication.addEventListener('activated', async function (args) {
+
+        var check = WinMLBridge.YoloModel.checkBridge();
+        console.log("Log:" + check);
+
+        document.getElementById("snaptabheader").addEventListener("click", function () { openTab('snaptab'); });
+        document.getElementById("uploadtabheader").addEventListener("click", function () { openTab('uploadtab'); });
+
+        openTab("snaptab");
+    });
+}
+
+function loadTab(tabName) {
+
+    switch (tabName) {
+
+        case "snaptab":
+            var video = startWebcam();
+            loadsnaptabElements(video);
+            break;
+        case "uploadtab":
+            loaduploadtabElements();
+            break;
+    } 
+
+    document.getElementById(tabName).style.display = "block";
+}
+
+async function loaduploadtabElements() {
+
+    // Trigger photo take
+    document.getElementById("uploadButton").addEventListener("click", async function() {      
+
+        var picker = new Windows.Storage.Pickers.FileOpenPicker();
+        picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+        picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+        picker.FileTypeFilter.Add(".jpg");
+        picker.FileTypeFilter.Add(".jpeg");
+        picker.FileTypeFilter.Add(".png");
+
+        var file = await picker.pickSingleFileAsync();
+        if (file != null) {
+
+            var base64Image = "";
+            var reader = new FileReader();
+
+            reader.addEventListener("load", function () {
+                preview.src = reader.result;
+            }, false);
+
+            if (file) {
+                base64Image = reader.readAsDataURL(file);
+            }
+
+            var model = await loadModel();
+           
+            var boxes = await model.evaluateModelAndProcessOutputAsync(base64Image);
+
+            renderImageOutput(boxes, image);
+        }
+        else {
+            // do nothing
+        }
+
+       
+    });
+}
+
+async function loadsnaptabElements(video) {
+    // Elements for taking the snapshot
+    var canvas = document.getElementById('canvas');
+    var canvasContext = canvas.getContext('2d');
+    var model = await loadModel();
+
+    // Trigger photo take
+    document.getElementById("snapButton").addEventListener("click", async function () {
+        canvasContext.drawImage(video, 0, 0, 640, 480);
+        var imageUrl = canvas.toDataURL("image/png");
+        var base64Image = imageUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+
+        var image = new Image();
+        image.src = imageUrl;
+       
+        var boxes = await model.evaluateModelAndProcessOutputAsync(base64Image);
+
+        renderImageOutput(boxes, image);
+    });
+}
+
+function startWebcam() {
+    var video = document.getElementById('video');
+
+    // Get access to the camera!
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // Not adding `{ audio: true }` since we only want video now
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        });
+    }
+
+    return video;
+}
 
 async function loadModel() {
     //var yolo2 = "ms-appx:///Models/tiny_yolov2/tiny_yolov2.onnx";
@@ -48,6 +124,7 @@ async function loadModel() {
 }
 
 function renderImageOutput(boxes, img) {
+
     var canvas = document.getElementById("outputCanvas");
     var ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0);
